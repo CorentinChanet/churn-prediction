41d0820413d77795d7465a7fcdafc9f75a1aaafd
Corty-Plato <corentin.chanet@gmail.com>
2021-08-12 14:07:49 +0200

    Add text for algorithms

--- main.py
+++ main.py
@@ -16,36 +16,46 @@ df = pd.concat([data, target], axis=1)
 #_clf_pipeline('LightGBM', df, balance=False, n_repeat=1)
 
 _features = sorted(df.columns.to_list())
+
 with st.sidebar:
     st.set_page_config(layout="wide", initial_sidebar_state='collapsed')
-    st.sidebar.title('Data Analysis Strategy')
-
-    selection_strategy = st.sidebar.selectbox('Please select an algorithm:',
-                                            ('RandomForest',
-                                             'BalancedRandomForest',
-                                             'KNN',
-                                             'LightGBM'))
+    st.sidebar.header('Algorithms')
+    selection_strategy = st.selectbox('Please select an algorithm:',
+                                              ('RandomForest',
+                                               'BalancedRandomForest',
+                                               'KNN',
+                                               'LightGBM'))
     st.markdown("""---""")
+    if selection_strategy == 'KNN':
+        st.markdown(f"""Neighbors-based classification is a type of instance-based learning 
+        or non-generalizing learning: it does not attempt to construct a general internal model, 
+        but simply stores instances of the training data. Classification is computed from a simple 
+        majority vote of the nearest neighbors of each point: a query point is assigned the data 
+        class which has the most representatives within the nearest neighbors of the point. <br></br>
+        <a href=https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html>From scikit-learn</a>""", unsafe_allow_html=True)
 
-    selection_x = st.sidebar.selectbox('Please select a feature for the X axis:',
-                                              _features, index=len(_features)-4)
+    elif selection_strategy == 'BalancedRandomForest':
+        st.markdown(f"""A balanced random forest randomly under-samples each boostrap sample to 
+        balance it. This class provides all functionality of the RandomForestClassifier. <br></br>
+        <a href=https://imbalanced-learn.org/stable/ensemble.html#forest>From imbalance-learn</a>""", unsafe_allow_html=True)
 
-    selection_y = st.sidebar.selectbox('Please select a feature for the Y axis:',
-                                       _features, index=len(_features)-2)
+    elif selection_strategy == 'RandomForest':
+        st.markdown(f"""A random forest is a meta estimator that fits a number of decision tree 
+        classifiers on various sub-samples of the dataset and uses averaging to improve the predictive 
+        accuracy and control over-fitting. This means a diverse set of classifiers is created by introducing 
+        randomness in the classifier construction. The prediction of the ensemble is given as the averaged 
+        prediction of the individual classifiers. <br></br>
+        <a href=https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html>From scikit-learn</a>""", unsafe_allow_html=True)
 
-    selection_z = st.sidebar.selectbox('Please select a feature for the Z axis:',
-                                       _features, index=len(_features)-1)
-
-    st.markdown("""---""")
-
-
+    elif selection_strategy == 'LightGBM':
+        st.markdown(f"""LightGBM is a gradient boosting framework that uses tree based learning algorithms. 
+        Many boosting tools use pre-sort-based algorithms(e.g. default algorithm in xgboost) for decision tree learning. 
+        It is a simple solution, but not easy to optimize. LightGBM uses histogram-based algorithms, which bucket continuous feature 
+        (attribute) values into discrete bins. This speeds up training and reduces memory usage.<br></br>
+        <a href=https://lightgbm.readthedocs.io/en/latest/Features.html>From LightGBM</a>""", unsafe_allow_html=True)
 
 mark = {True:"CORRECT", False:"WRONG"}
 
-features = selection_x, selection_y, selection_z
-confusion_matrix = get_confusion_mtx(selection_strategy)
-scatter_3D_balanced = plot_3D(balance_labels(df), features)
-scatter_3D_dataset = plot_3D(df, features)
 
 corr_matrix = get_corr(df)
 
@@ -56,39 +66,61 @@ corr_matrix = get_corr(df)
 
 metric('Corentin Chanet - becode.org 2021', "Explainable AI for Churn Prediction")
 
-expander_EDA = st.expander("Exploratory Data Analysis", expanded=False)
-with expander_EDA:
+st.markdown("Challenge statement and dataset available on <a href=https://www.kaggle.com/sakshigoyal7/credit-card-customers>Kaggle</a>", unsafe_allow_html=True)
+st.markdown("Code available on <a href=https://github.com/CorentinChanet/churn-prediction>GitHub</a>", unsafe_allow_html=True)
 
-    data, target = get_data_target(balance_labels(df.copy()))
+expander_EDA = st.expander("Exploratory Data Analysis", expanded=False)
+
+with expander_EDA:
 
     margin_0_left, original_table, margin_0_right = st.columns((0.1, 1, 0.1))
 
     with original_table:
-        st.subheader(f"Original Dataset")
         if st.checkbox("Show Dataframe", key='original_dataframe'):
             st.dataframe(df)
+        st.subheader(f"Original Dataset")
+
     st.write("\n")
     metric_row_custom({
         "NaN Values": (0, '', 'black', 'black', 'black', '30px'),
         "Features": (data.shape[1], '', 'black', 'black', 'black', '30px'),
-        "Attrited Customers": (target.value_counts()[0], '', 'black', 'red', 'black', '30px'),
-        "Remaining Customers": (len(target) - target.value_counts()[0], '', 'black', 'blue', 'black', '30px')
+        "Attrited Customers": (target.value_counts()[1], '', 'black', 'red', 'black', '30px'),
+        "Remaining Customers": (target.value_counts()[0], '', 'black', 'blue', 'black', '30px')
     })
 
     margin_1_left, clean_table, margin_1_right = st.columns((0.1, 1, 0.1))
 
     with clean_table:
-        st.subheader(f"Features dataset after cleaning, One Hot Encoding and manual downsampling")
         if st.checkbox("Show Dataframe", key='cleaned_dataframe'):
             st.dataframe(data)
+        st.subheader(f"Features dataset after cleaning, One Hot Encoding and manual undersampling")
+
+    data, target = get_data_target(balance_labels(df.copy()))
+
     st.write("\n")
     metric_row_custom({
         "NaN Values": (0, '', 'black', 'black', 'black', '30px'),
         "Features": (data.shape[1], '', 'black', 'black', 'black', '30px'),
-        "Attrited Customers": (target.value_counts()[0], '', 'black', 'red', 'black', '30px'),
-        "Remaining Customers": (len(target) - target.value_counts()[0], '', 'black', 'blue', 'black', '30px')
+        "Attrited Customers": (target.value_counts()[1], '', 'black', 'red', 'black', '30px'),
+        "Remaining Customers": (target.value_counts()[0], '', 'black', 'blue', 'black', '30px')
     })
 
+    row_eda_feature_x, row_eda_feature_y, row_eda_feature_z = st.columns((1, 1, 1))
+    with row_eda_feature_x:
+        selection_x = st.selectbox('Please select a feature for the X axis:',
+                                   _features, index=len(_features) - 4, key='selection_x_eda')
+    with row_eda_feature_y:
+        selection_y = st.selectbox('Please select a feature for the Y axis:',
+                                   _features, index=len(_features) - 2, key='selection_y_eda')
+    with row_eda_feature_z:
+        selection_z = st.selectbox('Please select a feature for the Z axis:',
+                                   _features, index=len(_features) - 1, key='selection_z_eda')
+
+    st.write('\n')
+    features = selection_x, selection_y, selection_z
+    confusion_matrix = get_confusion_mtx(selection_strategy)
+    scatter_3D_balanced = plot_3D(balance_labels(df), features)
+    scatter_3D_dataset = plot_3D(df, features)
 
     row_0_left, row_0_margin, row_0_right = st.columns((1, 0.1, 1))
 
@@ -185,6 +217,7 @@ with expander_classification:
         )
 
     if st.checkbox("Show SHAP diagnostics"):
+
         row_2_left, row_2_margin, row_2_right = st.columns((1, 0.1, 1))
 
         with row_2_left:
@@ -212,26 +245,28 @@ with expander_classification:
             st.markdown(f'<p style="font-family:IBM Plex Sans; color:black; font-size: 16px;">{explanation_text}</p>',
                         unsafe_allow_html=True)
 
-        with row_2_left:
+        st.write('\n')
+        row_class_feature_x, row_class_feature_y, row_class_feature_z = st.columns((1, 1, 1))
+        with row_class_feature_x:
+            selection_x = st.selectbox('Please select a feature for the X axis:',
+                                       _features, index=len(_features) - 4, key='selection_x_clf')
+        with row_class_feature_y:
+            selection_y = st.selectbox('Please select a feature for the Y axis:',
+                                       _features, index=len(_features) - 2, key='selection_y_clf')
+        with row_class_feature_z:
+            selection_z = st.selectbox('Please select a feature for the Z axis:',
+                                       _features, index=len(_features) - 1, key='selection_z_clf')
+
+
+        row_3_left, row_3_margin, row_3_right = st.columns((1, 0.1, 1))
+
+        with row_3_left:
             st.subheader(f'3D Plot of the Testing Set')
             selection_seed = st.slider("Pick a sample", 0, 100, 42)
             decision_plot, match, index, true_y, predict_y = shap_decision_plot(selection_strategy, selection_seed)
             scatter_3D_testing_set = plot_testing_set(selection_strategy, index, features)
             st.plotly_chart(scatter_3D_testing_set, use_container_width=True)
 
-        with row_2_right:
+        with row_3_right:
             st.subheader(f'SHAP random single-sample decision plot \n {mark[match]} prediction (TRUE : {true_y}) \n')
             st.write(decision_plot)
-
-# plt_corr = plot_corr(df)
-
-# plt_3D = plot_3D(df)
-
-
-
-
-
-
-
-#ari_kmeans, ari_dbscan = cluster(df)
-
